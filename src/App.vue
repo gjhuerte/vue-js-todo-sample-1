d0ead9f7e56f7fdf06e7236e874baeaa121a0399
Gabriel Jay Huerte <gjhuerte@gmail.com>
2019-05-02 13:00:01 +0800

    Added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,19 @@
 <template>
     <div id="app">
         <Header />
-        <AddTodo v-on:add-todo="addTodo" />
-        <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+        <router-view/>
     </div>
 </template>
 
 <script>
     import Header from './components/layout/Header';
-    import Todos from './components/Todos';
-    import AddTodo from './components/AddTodo.vue';
-    import axios from 'axios';
 
     export default {
-        name: 'app',
+        name: "app",
         components: {
-            Todos, 
-            Header, 
-            AddTodo
-        },
-    data() {
-        return {
-            todos: []
+            Header,
         }
-    },
-    methods: {
-        deleteTodo(id) {
-          axios.delete(`https://jsonplaceholder.typicode.com/todos/{id}`)
-            .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
-            .catch(err => console.log(err));
-        },
-
-      addTodo(newTodo) {
-          const { title, completed } = newTodo;
-
-          axios.post('https://jsonplaceholder.typicode.com/todos', {
-              title,
-              completed
-          }).then(res => this.todos = [ ...this.todos, res.data ])
-            .catch(err => console.log(err));
-      }
-    },
-    created() {
-        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-            .then(res => this.todos = res.data)
-            .catch(err => console.log(err));
     }
-  }
 </script>
 
 <style>
